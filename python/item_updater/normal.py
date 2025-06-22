4be090d497a2d2a9c5a03ece1c273e745489c435
santosh <santosh27144@gmail.com>
2025-06-22 11:53:33 +0530

    normal item updated added

---
+++ python/item_updater/normal.py
@@ -0,0 +1,8 @@
+from .base import ItemUpdater
+
+class NormalItemUpdater(ItemUpdater):
+    """Updater for normal items that degrade in quality over time."""
+    def update(self):
+        self.decrease_sell_in()
+        degrade = 1 if self.item.sell_in >= 0 else 2
+        self.decrease_quality(degrade)
